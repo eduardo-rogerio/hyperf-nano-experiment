46470a8ac8867bea07c591a6847dfdd4fb86da5b
eduardo-rogerio <eduardorogerio@protonmail.com>
2024-09-15 00:40:39 -0300

    feat() adiciona log.

--- app/Providers/ServiceProvider.php
+++ app/Providers/ServiceProvider.php
@@ -3,13 +3,25 @@
 namespace Mixd\App\Providers;
 
 use Hyperf\Contract\ContainerInterface;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use Psr\Log\LoggerInterface;
 
 class ServiceProvider
 {
-    public static function register(ContainerInterface $container)
+    public static function register(ContainerInterface $container): void
     {
 //        $container->set(DatabaseConfig::class, function () {
 //            return new DatabaseConfig();
 //        });
+        self::loadLogs($container);
+    }
+
+    public static function loadLogs(ContainerInterface $app): void
+    {
+        $logger = new Logger('hyperf');
+        $logFile = BASE_PATH . '/runtime/logs/hyperf.log';
+        $logger->pushHandler(new StreamHandler($logFile, 100));
+        $app->set(LoggerInterface::class, $logger);
     }
 }
